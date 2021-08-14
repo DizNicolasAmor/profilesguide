89bfc0b3e1136901303cf88d4572d2a53049539b
Nicolas Diz <diz.nicolasamor@gmail.com>
2021-08-14 14:43:57 -0300

    MASTER: frontend. create ResetPassword component

---
+++ frontend/src/components/resetPassword/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import CustomSection from "../common/customSection";
+
+const ResetPassword: FC = () => (
+  <CustomSection title="Reset password" titleId="idOfResetPasswordTitle">
+    <p className="text-center my-5">Here the user will reset the password</p>
+  </CustomSection>
+);
+
+export default ResetPassword;
